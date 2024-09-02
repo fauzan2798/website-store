62755ee4b5e1af069a460e6643acaf3f1c0fe5f9
risanjayy <risyadul@thelionparcel.com>
2024-09-02 10:50:45 +0700

    add page store category

--- components/info.tsx
+++ components/info.tsx
@@ -1,7 +1,10 @@
+"use client"
+
 import { Product } from "@/types"
 import Currency from "./ui/currency"
 import { Button } from "./ui/button"
 import { MessageCircleIcon } from "lucide-react"
+import Link from "next/link"
 
 interface InfoProps {
     data: Product
@@ -10,6 +13,13 @@ interface InfoProps {
 const Info: React.FC<InfoProps> = ({
     data
 }) => {
+    const URL = `${window.location.origin}/product/${data.id}`
+
+    const telp = process.env.NEXT_PUBLIC_TELP
+    const pesan = `Halo saya ingin membeli ${data.name} - ${data.price} dengan link: ${URL}`
+
+    const link = `https://wa.me/${telp}?text=${pesan}`
+
     return (
         <div>
             <h1 className="text-3xl font-bold text-gray-900">{data.name}</h1>
@@ -20,10 +30,15 @@ const Info: React.FC<InfoProps> = ({
             </div>
             <hr className="my-4"/>
             <div className="mt-10 flex items-center gap-x-3">
+                <Link
+                href={link}
+                target="_blank"
+                >
                 <Button className="flex items-center gap-x-2">
                     Chat Penjual
                     <MessageCircleIcon size={20} />
                 </Button>
+                </Link>
             </div>
         </div>
     )
