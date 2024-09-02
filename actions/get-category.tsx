3ff0962753197efbf243cc5f62d96cff11bcf7d3
risanjayy <risyadul@thelionparcel.com>
2024-09-02 14:06:10 +0700

    add product preview

---
+++ actions/get-category.tsx
@@ -0,0 +1,14 @@
+import { Category } from "@/types";
+
+const URL = `${process.env.PUBLIC_API_URL}/categories`
+
+
+const getCategory = async ( id: string ): Promise<Category> => {
+    const res = await fetch(`${URL}/${id}`)
+
+    const data = await res.json();
+  
+    return data;
+}
+
+export default getCategory
